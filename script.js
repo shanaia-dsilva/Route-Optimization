dde320984b791b832a24e72e9dff2bb772ba4dd9
Shanaia D'Silva <shanaia.dsilva30@gmail.com>
2025-06-15 23:00:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function uploadFile() {
     document.getElementById('resultBox').textContent = JSON.stringify(data.result, null, 2);
   });
 }
+
+var total_proj=0, optim_num=0, savings=0;
+var project_count=0;
+
+total_projects=document.getElementById('total-projects-num');;
+optim=document.getElementById('routes-num');
+save=document.getElementById('savings-num');
+
+total_projects.innerHTML=total_proj;
+optim.innerHTML=optim_num;
+save.innerHTML=savings+"%";
